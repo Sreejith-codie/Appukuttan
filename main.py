b7bf752c4e5be391dc50e9c0c2e45b588723ff6a
Sreejith R <sreejithsparkie@gmail.com>
2026-02-27 22:33:51 +0530

    Update bot start message to include 'on Render!'
    Signed-off-by: Sreejith R <sreejithsparkie@gmail.com>

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
-
 import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Bot is working!")
+    await update.message.reply_text("Bot is working on Render! 🔥")
 
 def main():
     app = ApplicationBuilder().token(BOT_TOKEN).build()
-    
+
     app.add_handler(CommandHandler("start", start))
 
     app.run_polling()
